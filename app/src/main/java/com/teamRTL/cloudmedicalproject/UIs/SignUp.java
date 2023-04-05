fe59304a422a030cbb9459698c334e745035f541
Reema <121347238+reemamaher201@users.noreply.github.com>
2023-04-05 14:30:05 +0300

    newfile

---
+++ app/src/main/java/com/teamRTL/cloudmedicalproject/UIs/SignUp.java
@@ -0,0 +1,16 @@
+package com.teamRTL.cloudmedicalproject.UIs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.teamRTL.cloudmedicalproject.R;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
